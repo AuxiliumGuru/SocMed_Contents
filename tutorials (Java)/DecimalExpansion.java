61ff1af6f88e9142d1782531a5ffff00cf054403
Khester Mesa <119793489+AuxiliumGuru@users.noreply.github.com>
2023-08-16 15:17:08 +0800

    Update DecimalExpansion.java

--- tutorials (Java)/DecimalExpansion.java
+++ tutorials (Java)/DecimalExpansion.java
@@ -4,9 +4,76 @@ public class DecimalExpansion {
 	public static void main(String[] args) {
 		System.out.println(bi_to_dec("101"));
 		System.out.println(oct_to_dec("303237"));
+		System.out.println(hex_to_dec("FACED"));
 	}
-// - Octal to Decimal -
- /**
+	
+	// - Hexadecimal to Decimal
+	
+	/**
+	 * Converts hexadecimal number to decimal number
+	 * @param hex ; The hexadecimal number to be converted
+	 * @return the equivalent decimal representation as a string
+	 */
+	public static String hex_to_dec(String hex) {
+		Integer decimal = 0;
+		char[] decimalArray = hex.toCharArray();
+		
+		for (int i = hex.length() -1; i >= 0; i--) {
+			int hexDigit = getDecEquivalent(decimalArray[i]);
+			int decDigit = (int) (hexDigit * Math.pow(16, hex.length()- i-1));
+			decimal += decDigit;
+		}
+		
+		return decimal.toString();
+	}
+	
+	
+	/**
+	 * Gets the equivalent Decimal representation of a hex char
+	 * @param hex
+	 * @return the equivalent Decimal representation as integer
+	 * @throws IllegalStateException - for invalid hex char
+	 */
+	public static int getDecEquivalent(char hex) {
+		switch(hex) {
+		case '1':
+			return 1;
+		case '2':
+			return 2;
+		case '3':
+			return 3;
+		case '4':
+			return 4;
+		case '5':
+			return 5;
+		case '6':
+			return 6;
+		case '7':
+			return 7;
+		case '8':
+			return 8;
+		case '9':
+			return 9;	
+		case 'A':
+			return 10;
+		case 'B':
+			return 11;
+		case 'C':
+			return 12;
+		case 'D':
+			return 13;
+		case 'E':
+			return 14;
+		case 'F':
+			return 15;
+		default:
+			throw new IllegalStateException("Invalid Hexadecimal Digit");	
+		}
+	}
+	
+	
+	// - Octal to Decimal -
+	 /**
      * Converts an octal number to a decimal number.
      *
      * @param octal The octal number to be converted (as a string).
