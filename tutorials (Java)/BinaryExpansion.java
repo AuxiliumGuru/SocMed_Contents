e2ce081d4c33c06054d365272dfffe12e83f6617
Khester Mesa <119793489+AuxiliumGuru@users.noreply.github.com>
2023-08-18 18:41:56 +0800

    Update BinaryExpansion.java

--- tutorials (Java)/BinaryExpansion.java
+++ tutorials (Java)/BinaryExpansion.java
@@ -1,14 +1,29 @@
+package tutorial;
+//import tutorial.DecimalExpansion;
+
 public class BinaryExpansion {
-	// Main function
-	public static void main(String[] args) {
-		System.out.println(binary_expansion(10));
-	}
+	
+	DecimalExpansion dec;
+	
+	
+	/**
+	 *  Method to convert a octal number to its binary representation
+	 * @param octal (Octal string value)
+	 * @return binary equivalent (string)
+	 */
+	 public String oct_to_bi(String octal) {
+		 // Create an instance of the DecimalExpansion class
+			dec = new DecimalExpansion();
+		 // returns binary representation of the given octal number
+			return dec_to_bi(Integer.parseInt(dec.oct_to_dec(octal)));
+		}
+	
 	/**
 	 *  Method to convert a decimal number to its binary representation
 	 * @param dec (Decimal value)
 	 * @return binary equivalent (string)
 	 */
-    public static String binary_expansion(int dec) {
+    public String dec_to_bi(int dec) {
         // StringBuilder to store the binary representation
         StringBuilder binary = new StringBuilder();
      
